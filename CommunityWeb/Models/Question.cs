5250bc5feb0719def85ae4e76c3205907ef5e765
TVYDev <vannyoutang23@gmail.com>
2017-12-27 11:33:39 +0700

    Create class model Question.

---
+++ CommunityWeb/Models/Question.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CommunityWeb.Models
+{
+    public class Question
+    {
+        public int Id { get; set; }
+
+        public ApplicationUser User { get; set; }
+
+        public string UserId { get; set; }
+
+        [StringLength(255)]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+
+        public string ImageUrls { get; set; }
+    }
+}
