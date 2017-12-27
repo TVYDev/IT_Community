5ecbd02051bc66359736650c086e9e9d571b71b8
TVYDev <vannyoutang23@gmail.com>
2017-12-27 10:25:00 +0700

    Initiate ASP.NET MVC project with .NET framework 4.6

---
+++ CommunityWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CommunityWeb.MvcApplication" Language="C#" %>
---
+++ CommunityWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CommunityWeb.Startup))]
+namespace CommunityWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
